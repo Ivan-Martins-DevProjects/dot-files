9ad6bb89f99dd9ed111af87ad02ceeda8b68861f
Ivan Martins <Ivan_G.Martins@outlook.com>
2026-09-19 18:30:28 -0300

    Script para copiar os arquivos rapidamente

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -rf ./nvim/ ~/.config/ && echo "Neovim configurado"
+
+cp -rf ./tmux/ ~/.config && echo "Tmux configurado"
+
+cp -rf ./bashrc/.bashrc ~/.bashrc && echo "Bash configurado"
